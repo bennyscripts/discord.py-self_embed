4253ea7977b17972de2e15de3606a183f70b22b0
Ben Tettmar <bentettmar@yahoo.com>
2022-03-31 07:36:53 +0100

    Shorten url example

---
+++ examples/shorten_url.py
@@ -0,0 +1,9 @@
+import discord_self_embed
+
+shortener = discord_self_embed.utils.Shortener("YOUR_BITLY_API_TOKEN")
+
+embed = discord_self_embed.Embed("discord.py-self_embed", description="A way for selfbots to send embeds again.", colour="ff0000", url="https://github.com/bentettmar/discord.py-self_embed")
+embed.set_author("Ben Tettmar")
+
+url = embed.generate_url(shorten_url=True, shortener=shortener)
+print(url)
